c0d216201de288e73c8e4339ad61f3b7a88da08d
Atul Kale <kaleatul9@gmail.com>
2021-03-08 16:18:48 +0530

    Hamburger js, style, html added . Errors solved.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
+// Hamburger
+var hamburger = document.querySelectorAll('.hamburger div');
+var close = document.getElementById('close');
 
+function menushow(e){
+  var div = e.target;
+  div.parentNode.nextElementSibling.classList.remove('ulhide');
+  div.parentNode.nextElementSibling.classList.add('ulshow');
+}
+
+function menuhide(e){
+  var div = e.target;
+  div.parentNode.classList.remove('ulshow');
+  div.parentNode.classList.add('ulhide');
+}
+
+for(div of hamburger){
+  div.addEventListener('click', menushow);
+}
+close.addEventListener('click', menuhide);
 
 
 
