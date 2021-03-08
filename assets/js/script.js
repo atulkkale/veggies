983546868d708d2db2e00ae6ad10da133e9baaa3
Atul Kale <kaleatul9@gmail.com>
2021-03-08 18:47:43 +0530

    Modal js function and css added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,52 @@
 var hamburger = document.querySelectorAll('.hamburger div');
 var close = document.getElementById('close');
 
-function menushow(e){
+function menushow(e) {
   var div = e.target;
   div.parentNode.nextElementSibling.classList.remove('ulhide');
   div.parentNode.nextElementSibling.classList.add('ulshow');
 }
 
-function menuhide(e){
+function menuhide(e) {
   var div = e.target;
   div.parentNode.classList.remove('ulshow');
   div.parentNode.classList.add('ulhide');
 }
 
-for(div of hamburger){
+for (div of hamburger) {
   div.addEventListener('click', menushow);
 }
 close.addEventListener('click', menuhide);
 
+// Modal
+var images = document.querySelectorAll('.special-images li');
 
+function toggleModal(e) {
+  var clickedElement = e.target.tagName;
+  console.log(clickedElement);
+  switch (clickedElement) {
+    case 'IMG':
+      e.preventDefault();
+      var li = e.target.parentNode.parentNode;
+      li.classList.add('makemodal');
+      break;
 
+    case 'A':
+      e.preventDefault();
+      var a = e.target.parentNode;
+      a.classList.toggle('makemodal');
+      break;
 
+    case 'LI':
+      var li = e.target;
+      li.classList.toggle('makemodal');
+      break;
 
+    default:
+      break;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (img of images) {
+  img.addEventListener('click', toggleModal);
+}
